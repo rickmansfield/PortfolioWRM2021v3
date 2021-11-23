6227997d16d742f92a45368e9a0571c469ee5f79
Rick <ricksmycodeguy@gmail.com>
2021-11-23 13:33:25 -0600

    added profile_pic to about.js

--- src/components/about.js
+++ src/components/about.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import './about.css'
-
+import Rick from '../photos/RickMansfield.PNG'
 export default function About() {
     const userInfo = {
-        profile_pic: 'https://picsum.photos/200/300?person', //use a square formatted image
+        profile_pic: Rick, //use a square formatted image
         bio: 'Full Stack Web Developer & Computer Science Engineer with a practical understanding of (HTML, CSS (responsive design), JavaScript, JSX, Node JS, React, Redux, Express, SQLite3, MySQL, Postgres, Python, MongoDB, and RESTful API & Responsive Design. Comfortable with New Features Implementation maintains and refactors existing code across various languages, frameworks, and databases. A growth mindset developer that works well with a team or independently in a remote setting',
     }
     
