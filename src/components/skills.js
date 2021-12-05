1faee497d2411beaa5b01f2871735b874fdeed31
Rick <ricksmycodeguy@gmail.com>
2021-12-05 14:17:43 -0600

    adds initial styled-components to skills.js

--- src/components/skills.js
+++ src/components/skills.js
@@ -1,12 +1,32 @@
 import React from 'react'
 import './skills.css'
+import styled from "styled-components";
 
 export default function skills() {
     return (
-        <div>
+        <StyledSkills>
+        <div id="Skills">
+            <h3 className='section_title'> - SKILLS - </h3>
             <p>Skills section under construction</p>
             <p>Coming Soon</p>
         </div>
+        </StyledSkills>
     )
 }
 
+const StyledSkills = styled.div`
+#Skills {
+    min-height: 100vh;
+    width: 100vw;
+    background-color: #001220;
+    display: flex;
+    flex-direction: column;
+}
+.section_title {
+    height: 10%;
+    color: var(--primary_color);
+    font-family: var(--accent_font);
+    font-size: 3.5rem;
+    margin: 25px auto;
+}
+`
