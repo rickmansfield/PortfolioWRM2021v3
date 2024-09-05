edcc2d15968a1601fb66775be660dca695a81608
Rick <ricksmycodeguy@gmail.com>
2024-09-05 17:51:04 -0500

    Adds State for conditioanl navigation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import './App.css';
+import { useState } from 'react';
 import About from './components/about';
 import Contact from './components/contact';
 import Nav1 from './components/Nav1';
 import Nav2 from './components/nav2';
 import Projects from './components/projects';
-import Skills from './components/skills'
+import Skills from './components/skills';
 
 function App() {
+  const [showNav2, setShowNav2] = useState(true);
+
+  const handleCheckboxChange = () => {
+    setShowNav2(prevState => !prevState); // Toggle between Nav1 and Nav2
+  };
+
   return (
     <div className="App">
-      <Nav2 />
-      <Nav1 />
+      {/* Pass the checkbox state and event handler to both Nav components */}
+      {showNav2 ? (
+        <Nav2 isChecked={showNav2} handleCheckboxChange={handleCheckboxChange} />
+      ) : (
+        <Nav1 isChecked={!showNav2} handleCheckboxChange={handleCheckboxChange} />
+      )}
       <Projects />
       <About />
       <Skills />
