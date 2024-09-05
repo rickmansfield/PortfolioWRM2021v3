f25b6cb8bcb75f729de011be4c136c2725421c61
Rick <ricksmycodeguy@gmail.com>
2024-09-05 10:06:34 -0500

    removes obsolete styled components styling

--- src/components/main.js
+++ src/components/main.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import './main.css';
-// import styled from 'styled-components';
-// import backGroundImg from '../photos/background.jpg'
 
 export default function Main() {
     const user = {
@@ -11,7 +9,6 @@ export default function Main() {
     }
 
     return (
-        // <StyleMain>
         <div id='Main'>
             <div className='main_background_img'>
                 <div className='main_links'>
@@ -27,74 +24,5 @@ export default function Main() {
                 </div>
             </div>
         </div>
-        // /* </StyleMain> */
     )
 }
-/*
-const StyleMain = styled.div`
-#Main {
-    height: 100vh;
-    width: 100%;
-}
-.main_background_img {
-    height: 100%;
-    width: 100%;
-    background-image: url(${backGroundImg});
-    background-size: cover;
-    background-position: center;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
-}
-
-.main_links {
-    width: 75%;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin: 10% auto 0;
-}
-
-.main_links a {
-    font-family: var(--accent_font);
-    color: var(--primary_color);
-    font-size: 1.5rem;
-}
-
-.main_text {
-    display: flex;
-    flex-direction: column;
-    margin: 50px auto 50vh;
-    color: var(--primary_color);
-}
-
-.main_text h1 {
-    font-size: 4rem;
-    font-family: var(--accent_font);
-
-}
-
-.main_text h2 {
-    font-size: 2rem;
-    font-family: var(--accent_font);
-}
-
-@media screen and (max-width: 414px) {
-    .main_links {
-        width: 75%;
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: space-between;
-        align-items: center;
-        margin: 10% auto 0;
-    }
-    
-    .main_links a {
-        width: 40%;
-        font-family: var(--accent_font);
-        color: var(--primary_color);
-        font-size: 1.5rem;
-        margin: 10px 5px;
-    }`;
-    */
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,69 +1,27 @@
 import React from 'react';
 import './nav.css';
-/*
-import styled from 'styled-components'
-const StyleNav = styled.div`
-#Nav {
-    height: 100px;
-    width: 100%;
-    background: var(--secondary_color);
-    display: none;
-    justify-content: space-between;
-    align-items: center;
-}
 
-.user_name {
-    height: 100%;
-    width: 50%;
-    display: flex;
-    align-items: center;
-    margin-left: 50px;
-}
-
-.user_name a {
-    font-family: var(--accent_font);
-    color: var(--primary_color);
-    font-size: 3rem;
-}
-
-.links {
-    width: 50%;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin: 0 50px;
-}
-
-.links a {
-    font-family: var(--accent_font);
-    color: var(--primary_color);
-    font-size: 1.2rem;
-}
-`
-*/
 export default function Nav() {
-    const user = {
-        name: 'Rick Mansfield', 
-        linkedIn: 'https://www.linkedin.com/in/peacefulrick/'
-    } 
+  const user = {
+    name: 'Rick Mansfield',
+    linkedIn: 'https://www.linkedin.com/in/peacefulrick/'
+  }
 
-    return (
-        //<StyleNav>
-        <nav id='Nav'>
-            <div className='user_name'>
-                <a href={user.linkedIn}>
-                    {user.name}
-                </a>
-                <p></p>
-            </div>
-            <div className='links'>
-                <a href='/'>Home</a>
-                <a href='#projects'>Project</a>
-                <a href='#about'>About</a>
-                <a href='#contact'>Contact</a>
-            </div>
-        </nav>
-       // </StyleNav>
-    )
+  return (
+    <nav id='Nav'>
+      <div className='user_name'>
+        <a href={user.linkedIn}>
+          {user.name}
+        </a>
+        <p></p>
+      </div>
+      <div className='links'>
+        <a href='/'>Home</a>
+        <a href='#projects'>Project</a>
+        <a href='#about'>About</a>
+        <a href='#contact'>Contact</a>
+      </div>
+    </nav>
+  )
 }
 
