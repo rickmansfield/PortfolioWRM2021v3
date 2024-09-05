65d077070f9a740fee0c84e601d6ac4b2251a34e
Rick <ricksmycodeguy@gmail.com>
2024-09-05 17:52:10 -0500

    Passes state and handleCheckboxChange

--- src/components/Nav1.js
+++ src/components/Nav1.js
@@ -1,17 +1,27 @@
-import React from 'react'
+import React from 'react';
 import './nav1.css';
 
-export default function Nav1() {
+export default function Nav1({ isChecked, handleCheckboxChange }) {
   const user = {
     name: 'Rick Mansfield',
     subtitle: 'Full Stack Web Development',
     subtitle2: '& Computer Science Engineer',
     linkedIn: 'https://www.linkedin.com/in/peacefulrick/TEST'
-  }
+  };
 
   return (
-    <div id='Main'>
-      <div className='main_background_img'>
+    <div id='nav1'>
+      <div className='nav1_background_img'>
+        <div className='user_info'>
+          <input
+            type='checkbox'
+            checked={isChecked}
+            onChange={handleCheckboxChange}
+          />
+          <a href={user.linkedIn}>
+            <h1>{user.name}</h1>
+          </a>
+        </div>
         <div className='main_links'>
           <a href='/'>Home</a>
           <a href='#Projects'>Projects</a>
@@ -20,12 +30,11 @@ export default function Nav1() {
         </div>
         <div className='main_text'>
           <a href={user.linkedIn}>
-          <h1>{user.name}</h1>
-          <h2>{user.subtitle}</h2>
+            <h2>{user.subtitle}</h2>
             <h2>{user.subtitle2}</h2>
           </a>
         </div>
       </div>
     </div>
-  )
+  );
 }
--- src/components/nav2.js
+++ src/components/nav2.js
@@ -1,27 +1,30 @@
 import React from 'react';
 import './nav2.css';
 
-export default function Nav2() {
+export default function Nav2({ isChecked, handleCheckboxChange }) {
   const user = {
     name: 'Rick Mansfield',
     linkedIn: 'https://www.linkedin.com/in/peacefulrick/TEST'
-  }
+  };
 
   return (
-    <nav id='Nav'>
-      <div className='user_name'>
+    <nav id='nav2'>
+      <div className='user_info'>
+        <input
+          type='checkbox'
+          checked={isChecked}
+          onChange={handleCheckboxChange}
+        />
         <a href={user.linkedIn}>
           {user.name}
         </a>
-        <p></p>
       </div>
-      <div className='links'>
-        <a href='/'>Home TEST ME</a>
-        <a href='#projects'>Project</a>
+      <div className='nav_links'>
+        <a href='/'>Home</a>
+        <a href='#projects'>Projects</a>
         <a href='#about'>About</a>
         <a href='#contact'>Contact</a>
       </div>
     </nav>
-  )
+  );
 }
-
