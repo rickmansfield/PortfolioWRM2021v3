1d62594077c43f60893db4a4f39338443d75d8cd
Rick <ricksmycodeguy@gmail.com>
2024-09-05 19:42:44 -0500

    Adds picture tag preps for device versitility

--- src/components/nav2.js
+++ src/components/nav2.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './nav2.css';
+import BackImg from '../photos/background.jpg';
 
 export default function Nav2({ isChecked, handleCheckboxChange, scrollToSection, sectionRefs }) {
   const user = {
@@ -8,23 +9,35 @@ export default function Nav2({ isChecked, handleCheckboxChange, scrollToSection,
   };
 
   return (
-    <nav id='nav2'>
-      <div className='user_info'>
-        <input
-          type='checkbox'
-          checked={isChecked}
-          onChange={handleCheckboxChange}
-        />
-        <a href={user.linkedIn}>
-          {user.name}
-        </a>
+    <>
+      <nav id='nav2'>
+        <div className='user_info'>
+          <input
+            type='checkbox'
+            checked={isChecked}
+            onChange={handleCheckboxChange}
+          />
+          <a href={user.linkedIn}>
+            {user.name}
+          </a>
+        </div>
+        <div className='nav_links'>
+          <button onClick={() => scrollToSection(sectionRefs.projectsRef)}>Projects</button>
+          <button onClick={() => scrollToSection(sectionRefs.aboutRef)}>About</button>
+          <button onClick={() => scrollToSection(sectionRefs.skillsRef)}>Skills</button>
+          <button onClick={() => scrollToSection(sectionRefs.contactRef)}>Contact</button>
+        </div>
+      </nav>
+      <div className="image-container">
+        <picture>
+          <source srcSet={BackImg} type="image/jpg" />
+          <img src={BackImg} alt="Background" className="background-img" />
+        </picture>
+        <div className="overlay-text">
+          <h2>Clean code. Efficient solutions.</h2>
+          <p>Ready to build the future</p>
+        </div>
       </div>
-      <div className='nav_links'>
-        <button onClick={() => scrollToSection(sectionRefs.projectsRef)}>Projects</button>
-        <button onClick={() => scrollToSection(sectionRefs.aboutRef)}>About</button>
-        <button onClick={() => scrollToSection(sectionRefs.skillsRef)}>Skills</button>
-        <button onClick={() => scrollToSection(sectionRefs.contactRef)}>Contact</button>
-      </div>
-    </nav>
+    </>
   );
 }
